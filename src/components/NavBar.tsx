789f99f351d2830b0bb7210c06f5034f2d03897a
Dave Trost <davetrost@gmail.com>
2020-02-14 14:34:41 -0800

    moving backwards ... all imports need "require"

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+// import HOMEicon from '../assets/HOMEicon.png';
+// import PROFILEicon from '../assets/PROFILEicon.png';
+// import SETTINGSicon from '../assets/SETTINGSicon.png';
+// import FLOWERicon from '../assets/FLOWERicon.png';
+// import styles from './NavBar.css';
+
+const { useLogout } = require('../utils/WithSession');
+const styles = require('./NavBar.css');
+const HOMEicon = require('../assets/HOMEicon.png');
+const PROFILEicon = require('../assets/PROFILEicon.png');
+const SETTINGSicon = require('../assets/SETTINGSicon.png');
+const FLOWERicon = require('../assets/FLOWERicon.png');
+
+const NavBar = () => {
+  const logout = useLogout();
+
+  return (
+    <div className={styles.NavBar}>
+      <button onClick={logout}>Log Out</button>
+      <span className={styles.NavigationFooter}>
+        <Link to="/"><img src={HOMEicon} alt="Home" /></Link>&nbsp;
+        <Link to="/profile"><img src={PROFILEicon} alt="Profile" /></Link>
+        <Link to="/settings"><img src={SETTINGSicon} alt="Settings" /></Link>
+        <Link to="/about-us"><img src={FLOWERicon} alt="About Us" /></Link>
+      </span>
+    </div>
+  );
+};
+
+export default NavBar;
