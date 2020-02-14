66c6ed5f317f6d382875cc0c350fc5542400e96a
Dave Trost <davetrost@gmail.com>
2020-02-14 14:02:16 -0800

    convert progress bar to TS

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Progress.css';
+import * as React from 'react';
+const styles = require('./Progress.css');
+
+export interface IProgressProps { 
+  now: number; 
+}
 
 /* Helpful tutorial on progress bars: 
 https://medium.com/@ItsMeDannyZ/how-to-build-a-progress-bar-with-react-8c5e79731d1f
 https://codepen.io/DZuz14/pen/oqeMpY?editors=0100
 */
-const Progress = ({ now }) => (
+const Progress = ({ now }: IProgressProps) => (
   <div className={styles.ProgressBar}>
     <div className={styles.ProgressFill} style={{ width: `${now}%` }} />
   </div>
 );
-Progress.propTypes = { now: PropTypes.number.isRequired };
 
 export default Progress;
